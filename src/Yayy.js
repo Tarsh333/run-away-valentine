7c4265b6c5ae2bcedb601dce53d8959e8f0714fc
Tarsh <tarshtarsh333@gmail.com>
2026-02-07 03:08:35 +0530

    Update Yayy.js

--- src/Yayy.js
+++ src/Yayy.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './Yayy.css';
 
 const Yayy = () => {
   return (
-    <div className='please' style={{marginTop:'50px'}}>
-    <h1>Yayy! Lets go on a date!!!!!</h1>
-    <img src="https://i0.wp.com/media0.giphy.com/media/2lQCBjuFMLCOvXno4l/giphy.gif?ssl=1"/>
-    </div>
-  )
-}
+    <div className="yayy-page">
+      <div className="floating-hearts">💖💘💝</div>
 
-export default Yayy
+      <h1 className="yayy-title">She said YES 💕</h1>
+
+      <p className="yayy-subtitle">This just became my favorite love story.</p>
+
+      <div className="photo-row">
+        <img src="/us1.jpeg" alt="Us 1" />
+        <img src="/us2.jpeg" alt="Us 2" />
+        <img src="/us3.jpeg" alt="Us 3" />
+      </div>
+
+      <p className="yayy-message">
+        I can’t wait to make more memories with you 💗 Dates, laughs, late
+        talks, and everything in between.
+      </p>
+
+      <p className="yayy-footer">See you soon, Valentine ❤️</p>
+    </div>
+  );
+};
+
+export default Yayy;
