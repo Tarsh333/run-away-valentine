0ac20b0b3672a687d06bad918d343e489f4cef98
Tarsh <tarshtarsh333@gmail.com>
2026-02-07 03:07:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Please from './Please';
 import Yayy from './Yayy';
+import ValentineLanding from './ValentineLanding';
 import './App.css';
 import details from './Email';
 import emailjs from 'emailjs-com';
 
 const App = () => {
-    const [yes, setYes] = useState(null);
-    useEffect(() => {
-      if(localStorage.getItem('yes!!')!=null){
-        setYes(true);
-      }
-    }, [])
-    const lesgoo=(e)=>{
-        e.preventDefault();
-        setYes(true);
-        emailjs.sendForm(`Steins;Gate`, details.TEMPLATE_ID, e.target, details.USER_ID)
-            .then((result) => {
-                alert("Yayyyyyyy!!!!!!!!", result.text);
-            },
-                (error) => {
-                    alert("An error occurred, Please try again", error.text);
-                });
-        localStorage.setItem('yes!!','yayy!');
-    }
-    
-  return (
-    <div className='home'>
-        {yes==null?<Please lesgoo={lesgoo}/>
-        :<Yayy/>}
-    </div>
-  )
-}
+  const [yes, setYes] = useState(null);
+  const [landingDone, setLandingDone] = useState(false);
 
-export default App
+  useEffect(() => {
+    if (localStorage.getItem('yes!!') != null) {
+      setYes(true);
+    }
+  }, []);
+
+  const lesgoo = (e) => {
+    e.preventDefault();
+    setYes(true);
+
+    emailjs
+      .sendForm(`Steins;Gate`, details.TEMPLATE_ID, e.target, details.USER_ID)
+      .then(
+        (result) => {
+          alert('Yayyyyyyy!!!!!!!!');
+        },
+        (error) => {
+          alert('An error occurred, Please try again');
+        }
+      );
+
+    localStorage.setItem('yes!!', 'yayy!');
+  };
+
+  // ❤️ SHOW LANDING FIRST
+  if (!landingDone) {
+    return <ValentineLanding onComplete={() => setLandingDone(true)} />;
+  }
+
+  // 👉 EXISTING APP CONTINUES
+  return (
+    <div className="home">
+      {yes == null ? <Please lesgoo={lesgoo} /> : <Yayy />}
+    </div>
+  );
+};
+
+export default App;
